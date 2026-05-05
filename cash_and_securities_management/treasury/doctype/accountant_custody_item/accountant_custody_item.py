28e9a19566f161a62c012ef0148fdf90c2331b3e
alriwaq <alriwaq.alarabi@gmail.com>
2026-05-05 22:52:54 +0300

    Refactor AccountantCustody class and methods

--- cash_and_securities_management/treasury/doctype/accountant_custody_item/accountant_custody_item.py
+++ cash_and_securities_management/treasury/doctype/accountant_custody_item/accountant_custody_item.py
@@ -1,22 +1,27 @@
 import frappe
 from frappe.model.document import Document
-def validate(self):
-    self._set_child_defaults()
 
 
-def _set_child_defaults(self):
-    for item in self.custody_items:
-        # Project fallback
-        if not item.project:
-            item.project = self.project
+class AccountantCustody(Document):
 
-        # Cost center fallback
-        if not item.cost_center:
-            item.cost_center = self.cost_center
+    def validate(self):
+        self._set_child_defaults()
 
-        # Warehouse fallback
-        if not item.warehouse:
-            item.warehouse = self.warehouse
+    def _set_child_defaults(self):
+        for item in self.custody_items:
+
+            # Project fallback
+            if not item.project:
+                item.project = self.project
+
+            # Cost center fallback
+            if not item.cost_center:
+                item.cost_center = self.cost_center
+
+            # Warehouse fallback
+            if not item.warehouse:
+                item.warehouse = self.warehouse
+
 
 class AccountantCustodyItem(Document):
-	pass
+    pass
